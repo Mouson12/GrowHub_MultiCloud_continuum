e13f48fe264768250352621b29e78cd5fc929ab0
Jakub Madry <jaiwiecejmnie@gmail.com>
2024-12-18 22:45:05 +0100

    Show proper icon & control cubit logic

--- app/growhub/lib/features/api/cubit/device/device_cubit.dart
+++ app/growhub/lib/features/api/cubit/device/device_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:growhub/features/api/api_repository.dart';
 import 'package:growhub/features/api/data/models/device_model.dart';
+import 'package:growhub/features/device_dashboard/entities/device_icon.dart';
 part 'device_state.dart';
 
 class DeviceCubit extends Cubit<DeviceState> {
@@ -29,6 +30,68 @@ class DeviceCubit extends Cubit<DeviceState> {
       emit(DeviceStateLoaded(devices));
     } catch (e) {
       emit(DeviceStateError(error: e.toString()));
+    }
+  }
+
+  Future<void> updateDevice({
+    required int deviceId,
+    DeviceIcon? icon,
+    String? location,
+    String? name,
+  }) async {
+    final state = this.state;
+
+    if (state is! DeviceStateLoaded) {
+      return;
+    }
+    emit(DeviceStateLoading(devices: state.devices));
+
+    try {
+      final token = await apiRepository.getToken();
+      if (token == null) {
+        emit(DeviceStateInitial());
+        return;
+      }
+
+      await apiRepository.updateDevice(
+        token: token,
+        deviceId: deviceId,
+        icon: icon,
+        location: location,
+        name: name,
+      );
+
+      // Znajdź urządzenie w aktualnym stanie
+      final device = findDeviceById(state.devices, deviceId);
+      if (device == null) {
+        emit(DeviceStateError(error: "No devices found."));
+        return;
+      }
+
+      final updatedDevice = device.copyWith(
+        icon: icon ?? device.icon,
+        name: name ?? device.name,
+      );
+
+      final updatedDevices = {
+        for (final d in state.devices)
+          if (d.id == deviceId) updatedDevice else d,
+      };
+
+      print(icon);
+
+      emit(DeviceStateLoaded(updatedDevices));
+    } catch (e) {
+      emit(DeviceStateError(error: e.toString()));
+    }
+  }
+
+  DeviceModel? findDeviceById(Set<DeviceModel> devices, int id) {
+    try {
+      return devices.firstWhere((device) => device.id == id);
+    } catch (e) {
+      // Jeśli urządzenie nie zostanie znalezione, zwróć null
+      return null;
     }
   }
 }
--- app/growhub/lib/views/settings/settings_page.dart
+++ app/growhub/lib/views/settings/settings_page.dart
@@ -1,39 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:growhub/config/assets.gen.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:growhub/common/widgets/progress_indicator.dart';
 import 'package:growhub/config/constants/colors.dart';
-import 'package:growhub/features/device_dashboard/entities/device_icon.dart';
+import 'package:growhub/features/api/cubit/device/device_cubit.dart';
+import 'package:growhub/features/api/data/models/device_model.dart';
 import 'package:growhub/features/pop-up/icon-choice/widgets/icon_pop_up.dart';
 
 class SettingsPage extends HookWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({
+    super.key,
+    required this.deviceId,
+  });
+
+  final int deviceId;
+
+  Set<DeviceModel>? getDevices(DeviceState from) {
+    final state = from;
+
+    if (state is DeviceStateLoaded) {
+      return state.devices;
+    }
+
+    if (state is DeviceStateLoading) {
+      return state.devices;
+    }
+
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      child: GestureDetector(
-        onTap: () {
-          showIconPopupDialog(
-            context,
-            IconPopUp(
-              startIcon: DeviceIcon.cactus,
-              onIconSelected: (icon) {
-                print(icon);
+    print(deviceId);
+    return Scaffold(
+      backgroundColor: GHColors.background,
+      body: BlocBuilder<DeviceCubit, DeviceState>(
+        builder: (context, state) {
+          if (getDevices(state) == null) {
+            return const GHProgressIndicator();
+          }
+          final device = context
+              .watch<DeviceCubit>()
+              .findDeviceById(getDevices(state)!, deviceId);
+
+          if (device == null) {
+            return const GHProgressIndicator();
+          }
+
+          return Center(
+            child: GestureDetector(
+              onTap: () {
+                showIconPopupDialog(
+                  context,
+                  IconPopUp(
+                    startIcon: device.icon,
+                    onIconSelected: (icon) {
+                      print(icon);
+                      context
+                          .read<DeviceCubit>()
+                          .updateDevice(deviceId: deviceId, icon: icon);
+                    },
+                  ),
+                );
               },
+              child: SizedBox(
+                width: 50,
+                height: 50,
+                child: SvgPicture.asset(
+                  device.icon.path,
+                  fit: BoxFit.contain,
+                  colorFilter:
+                      ColorFilter.mode(GHColors.black, BlendMode.srcIn),
+                ),
+              ),
             ),
           );
         },
-        child: SizedBox(
-          width: 50,
-          height: 50,
-          child: Assets.kwiatki.daisySvgrepoCom.svg(
-            height: 24,
-            colorFilter: ColorFilter.mode(
-              GHColors.black,
-              BlendMode.srcIn,
-            ),
-          ),
-        ),
       ),
     );
   }
